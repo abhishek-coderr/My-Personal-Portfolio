9a32b8d24b3cf7738fd3420ae7f49ac1a15a96af
Abhishek <122552273+abhishek-coderr@users.noreply.github.com>
2023-04-25 21:11:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -229,21 +229,21 @@ function status(){
 }
 
 // Preventing Inspect and right click
-document.addEventListener('contextmenu', event => event.preventDefault());
-document.onkeydown = (e) => {
-  if (e.key == 123) {
-      e.preventDefault();
-  }
-  if (e.ctrlKey && e.shiftKey && e.key == 'I') {
-      e.preventDefault();
-  }
-  if (e.ctrlKey && e.shiftKey && e.key == 'C') {
-      e.preventDefault();
-  }
-  if (e.ctrlKey && e.shiftKey && e.key == 'J') {
-      e.preventDefault();
-  }
-  if (e.ctrlKey && e.key == 'U') {
-      e.preventDefault();
-  }
-};
+// document.addEventListener('contextmenu', event => event.preventDefault());
+// document.onkeydown = (e) => {
+//   if (e.key == 123) {
+//       e.preventDefault();
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+//       e.preventDefault();
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+//       e.preventDefault();
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+//       e.preventDefault();
+//   }
+//   if (e.ctrlKey && e.key == 'U') {
+//       e.preventDefault();
+//   }
+// };
